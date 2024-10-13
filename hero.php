5189253633e2ad6e8f2ed24a0b8bcec9579d4ae2
Veren <157016054+veren123@users.noreply.github.com>
2024-10-13 13:53:08 +0700

    Add files via upload

---
+++ hero.php
@@ -0,0 +1,20 @@
+<?php
+
+$namaDia = "Minerva";
+$Umurbeliau ="19";
+
+
+switch ($Umurbeliau) {
+    case '':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+// if ($Umurbeliau < 13){
+//     echo $namaDia."DIa bukan orang yang sama untuk umurnya" ;
+// }else if ($Umurbeliau > 13){
+//     echo $namaDia."Waw dia udah dewasa ya ternyata umurnya aja" ;
+// }
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$namaTemanTerbaik="Minerva";
+$HobbynyaAdalah ="Gambar,maingame";
+
+
+
+echo "Nama orang terbaikku adalah :".$namaTemanTerbaik."Hobbynya ngapain ketika di rumah:".$HobbynyaAdalah;
+?>
+
